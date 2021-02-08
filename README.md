102b13ac14cad369e55aa6d7e7732c5d003c1319
Marcelo Costa <marceloc@uchicago.edu>
2021-02-08 09:22:11 -0600

    tweak README and introduce ssh instructions

--- README.md
+++ README.md
@@ -19,6 +19,17 @@ aws_access_key_id=<your_access_key_here>
 aws_secret_access_key=<your_secret_key_here>
 ```
 
+Please pick the credentials from an user with the appropriate permissions to create the required AWS Resources (Ec2, EBS Volumes, Security Groups, Launch Configuration and Auto Scaling Groups). More information [here](https://aws.amazon.com/iam/features/manage-permissions/).
+
+### Environments configuration and how to enable user access to the VMs
+
+To create an ssh key pair and allow access to your own users, just run `ssh-keygen -f ~/my-user-ssh-key-file -t rsa -b 4096` and replace the public keys found under the `users:` block of the `environments.yaml` (Just copy the contents of your `*.pub` file).
+
+These keys will be automatically added to the EC2 VMs' `/home/{user}/.ssh/authorized_keys` file. Once the VM is up and running, you can just follow the instructions on the console which should provide a command similar to:
+```
+ssh -i ~/user1-ssh-key-file user1@<public_ip_address_from_the_ec2_vm>
+```
+
 ## How to run
 
 The commands below will install the dependencies and execute the tool (which should seamlessly identify the AWS credentials placed in `~/.aws/credentials`):
--- mprovisioner/cloud/aws.py
+++ mprovisioner/cloud/aws.py
@@ -1,5 +1,7 @@
 import boto3
+import botocore
 from cloud.api import APIHandler
+import time
 import logging
 import os
 
@@ -13,51 +15,66 @@ class AWSAPIHandler(APIHandler):
     API Handler for AWS SDK interactions
     """
 
-    def provision_vms(self):
-        """
-        Creates EC2 VMs (through an Auto Scaling Group) according to the settings specified in environments.yaml
-        """
-        log.debug(f"environment configuration: {self.environment_config}")
+    def _get_asg(self, asg_client):
+        asg_lookup = asg_client.describe_auto_scaling_groups(
+            AutoScalingGroupNames=["MProvisionerASG1",]
+        )
+        asgs = asg_lookup["AutoScalingGroups"]
+        if len(asgs) > 0:
+            return asgs[0]
+        else:
+            return None
 
+    def _delete_existing_resources(self, asg_client, ec2_client):
+        """
+        Deletes existing resources to keep the automation idempotent
+        """
+        # TODO: Parameterize another instruction in case the user just wants to skip already-provisioned resources
+        try:
+            asg_deletion_result = asg_client.delete_auto_scaling_group(
+                AutoScalingGroupName="MProvisionerASG1", ForceDelete=True
+            )
+            log.debug(f"Auto Scaling Group deletion result: {asg_deletion_result}")
+        except botocore.exceptions.ClientError as ve:
+            log.warn(ve)
+
+        try:
+            launch_config_deletion_result = asg_client.delete_launch_configuration(
+                LaunchConfigurationName="MProvisionerLC1"
+            )
+            log.debug(
+                f"Launch Configuration deletion result: {launch_config_deletion_result}"
+            )
+        except botocore.exceptions.ClientError as ve:
+            log.warn(ve)
+
+        # Make sure the old MProvisionerASG1 is fully deleted
+        for i in range(0, 10):
+            lingering_asg = self._get_asg(asg_client)
+            if lingering_asg:
+                log.info("Still working on the deletion of ASG MProvisionerASG1...")
+                time.sleep(10)
+            else:
+                break
+
+        try:
+            sg_deletion_result = ec2_client.delete_security_group(
+                GroupName="MProvisionerSG1"
+            )
+            log.debug(f"Security Group deletion result: {sg_deletion_result}")
+        except botocore.exceptions.ClientError as ve:
+            log.warn(ve)
+
+    def _find_ami_id(self, ec2_client):
+        """
+        Identify the most recently-created AMI that matches the specs defined in environments.yaml
+
+        Return:
+            the AMI ID
+        """
         ami_type = self.environment_config["ami_type"]
         virtualization_type = self.environment_config["virtualization_type"]
         architecture = self.environment_config["architecture"]
-
-        # TODO: This function is too long
-        # the user-data, disks and the ami discovery logic must be moved to separate functions
-
-        # users
-        user_data_commands = ["#!/usr/bin/env bash"]
-        for u in self.environment_config["users"]:
-            ulogin = u["login"]
-            usshkey = u["ssh_key"]
-            log.debug(f"preparing configuration to add public key from user {ulogin}")
-            user_data_commands.append(f"useradd -m {ulogin}")
-            user_data_commands.append(
-                f'echo "{usshkey}" > /home/{ulogin}/.ssh/authorized_keys'
-            )
-
-        # disks
-        block_device_mappings = []
-        for d in self.environment_config["volumes"]:
-            ddevice = d["device"]
-            dsize_gb = d["size_gb"]
-            dtype = d["type"]
-            dmount = d["mount"]
-
-            disk = {
-                "DeviceName": ddevice,
-                "Ebs": {
-                    "VolumeSize": dsize_gb,
-                    "VolumeType": "gp2",
-                    "DeleteOnTermination": True,
-                    "Iops": 100,
-                },
-            }
-            # format disk according to the desired filesystem specified in environments.yaml
-            user_data_commands.append(f"sudo mkfs -t {dtype} {ddevice}")
-
-            block_device_mappings.append(disk)
 
         # the following query is equivalent to the CLI command:
         # % aws ec2 describe-images --owners self amazon --filters \
@@ -66,7 +83,7 @@ class AWSAPIHandler(APIHandler):
         #    Name=virtualization-type,Values=hvm \
         #    Name=owner-id,Values= 137112412989\ # This is Amazon's Owner ID
         #    Name=name,Values=\*amzn2\*
-        ec2_client = boto3.client("ec2", region_name="us-east-2")
+
         amis_search_result = ec2_client.describe_images(
             Filters=[
                 {
@@ -97,25 +114,114 @@ class AWSAPIHandler(APIHandler):
         log.info(f"Latest AMI found among the search results: {amis[0]}")
         log.debug(f"Selected AMI ID: {ami_id}")
 
-        # Creating a Launch Configuration to be used in an AutoScaling Group
-        asg_client = boto3.client("autoscaling", region_name="us-east-2")
+        return ami_id
 
-        # Idempotent configuration management (delete launch configuration if it already exists)
-        # TODO: Parameterize another instruction in case the user just wants to skip already-provisioned resources
-        launch_config_deletion_result = asg_client.delete_launch_configuration(
-            LaunchConfigurationName="MProvisionerLC1"
-        )
-        log.debug(
-            f"Launch Configuration deletion result: {launch_config_deletion_result}"
-        )
+    def provision_vms(self):
+        """
+        Creates EC2 VMs (through an Auto Scaling Group) according to the settings specified in environments.yaml
+        """
+        log.debug(f"environment configuration: {self.environment_config}")
 
+        session = boto3.Session(profile_name="default")
+        default_region = session.region_name
+        log.info(f"utilizing default region {default_region}")
+
+        # TODO: This function is too long (could use some refactoring)
+
+        # Creating Boto3 Client objects to provision resources and interact with these services
+        ec2_client = boto3.client("ec2", region_name=default_region)
+        asg_client = boto3.client("autoscaling", region_name=default_region)
+
+        # users
+        user_data_commands = ["#!/usr/bin/env bash"]
+        for u in self.environment_config["users"]:
+            ulogin = u["login"]
+            usshkey = u["ssh_key"]
+            log.debug(f"preparing configuration to add public key from user {ulogin}")
+            user_data_commands.append(f"sudo useradd -m {ulogin}")
+            user_data_commands.append(f"sudo mkdir /home/{ulogin}/.ssh")
+            user_data_commands.append(
+                f"sudo sh -c 'sudo echo \"{usshkey}\" > /home/{ulogin}/.ssh/authorized_keys'"
+            )
+
+        # disks
+        # TODO: Introduce permanent config to /etc/fstab
+        block_device_mappings = []
+        for d in self.environment_config["volumes"]:
+            ddevice = d["device"]
+            dsize_gb = d["size_gb"]
+            dtype = d["type"]
+            dmount = d["mount"]
+
+            disk = {
+                "DeviceName": ddevice,
+                "Ebs": {
+                    "VolumeSize": dsize_gb,
+                    "VolumeType": "gp2",
+                    "DeleteOnTermination": True,
+                    "Iops": 100,
+                },
+            }
+            # format disk according to the desired filesystem specified in environments.yaml
+            user_data_commands.append(f"sudo mkfs -t {dtype} {ddevice}")
+            user_data_commands.append(f"sudo mkdir {dmount}")
+            user_data_commands.append(f"sudo mount {ddevice} {dmount}")
+
+            block_device_mappings.append(disk)
+
+        # find AMI ID according to specs
+        ami_id = self._find_ami_id(ec2_client)
+
+        # BEGIN -- Idempotent configuration management (delete resources if they already exist)
+        self._delete_existing_resources(asg_client, ec2_client)
+
+        # Identify Default VPC (Every AWS account has one default VPC for each AWS Region)
+        # TODO: Allow user to specify different VPCs / multiple AZs / Subnets per environment
+        vpc_lookup_result = ec2_client.describe_vpcs(
+            Filters=[{"Name": "instance-tenancy", "Values": ["default"]}]
+        )
+        log.debug(f"Looking for the default VPC: {vpc_lookup_result}")
+        vpcs = vpc_lookup_result["Vpcs"]
+        if len(vpcs) > 0:
+            default_vpc_id = vpc_lookup_result["Vpcs"][0]["VpcId"]
+        else:
+            raise Exception(
+                "Could not find a default VPC on region {default_region}! Abort provisioning."
+            )
+
+        # Create security group to allow inbound SSH connection
+        try:
+            security_group_creation_result = ec2_client.create_security_group(
+                GroupName="MProvisionerSG1",
+                Description="allow inbound SSH traffic",
+                VpcId=default_vpc_id,
+            )
+            security_group_id = security_group_creation_result["GroupId"]
+            ec2_client.authorize_security_group_ingress(
+                GroupId=security_group_id,
+                IpPermissions=[
+                    {
+                        "IpProtocol": "tcp",
+                        "FromPort": 22,
+                        "ToPort": 22,
+                        "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
+                    }
+                ],
+            )
+            log.info("Security Group created successfully!")
+        except botocore.exceptions.ClientError as e:
+            raise Exception("Could not create security group! Abort provisioning.")
+
+        # Creating a Launch Configuration to be used by the AutoScaling Group
         asg_client.create_launch_configuration(
             LaunchConfigurationName="MProvisionerLC1",
             ImageId=ami_id,
             InstanceType=self.environment_config["instance_type"],
             UserData="\n".join(user_data_commands),
+            SecurityGroups=[security_group_id,],
             BlockDeviceMappings=block_device_mappings,
         )
+        log.info("Launch Configuration created successfully!")
 
         # Creating an Auto Scaling Group to control the fleet of EC2s
         asg_client.create_auto_scaling_group(
@@ -123,9 +229,36 @@ class AWSAPIHandler(APIHandler):
             MinSize=self.environment_config["min_count"],
             MaxSize=self.environment_config["max_count"],
             LaunchConfigurationName="MProvisionerLC1",
-            AvailabilityZones=["us-east-2a", "us-east-2b",],
+            AvailabilityZones=[f"{default_region}a", f"{default_region}b",],
         )
+        log.info("Auto Scaling Group created successfully!")
 
-        asg_dict = asg_client.describe_auto_scaling_groups()
-        for asg in asg_dict["AutoScalingGroups"]:
-            print(asg["AutoScalingGroupName"])
+        # TODO: Polling logic to determine when the EC2 instances are fully up and running (completed checks)
+        log.info("Waiting for Ec2 instances...")
+        time.sleep(15)
+
+        # Find the newly-provisioned ASG, fetch its instances and print ssh instructions to the user
+        new_asg = self._get_asg(asg_client)
+        log.info(f"Fetching instances from ASG {new_asg}...")
+        instance_ids = []
+        for ec2_instance in new_asg["Instances"]:
+            instance_ids.append(ec2_instance["InstanceId"])
+
+        # Printing ssh instructions
+        log.info(f"Use the following commands to ssh to the newly-provisioned VMs:")
+        print(
+            "ssh -i <private_key_from_your_user> <user>@<ec2_instance_public_ip_address>"
+        )
+        print("e.g.,")
+        for eid in instance_ids:
+            ec2_describe_instances_result = ec2_client.describe_instances(
+                Filters=[{"Name": "instance-id", "Values": [eid]}]
+            )
+            ec2_public_dns_name = ec2_describe_instances_result["Reservations"][0][
+                "Instances"
+            ][0]["PublicDnsName"]
+            print(
+                "ssh -i user1-ssh-key-file {}@{}".format(
+                    self.environment_config["users"][0]["login"], ec2_public_dns_name
+                )
+            )
